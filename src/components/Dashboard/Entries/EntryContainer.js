60f6a1b33ee878bf1f352672f76abfeb51889945
Mike Capecci <mjcapecci@gmail.com>
2020-12-16 20:47:13 -0600

    adds oredring functionality for entries

--- src/components/Dashboard/Entries/EntryContainer.js
+++ src/components/Dashboard/Entries/EntryContainer.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import firebase from 'gatsby-plugin-firebase';
 import { useCollectionOnce } from 'react-firebase-hooks/firestore';
+import { getUser } from '../../../utils/auth';
 
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import EntryItem from './EntryItem';
 
 const EntryLog = () => {
-  const uid = JSON.parse(localStorage.getItem('user')).uid;
+  const [sortedData, setSortedData] = useState(null);
   const [value, loading, error] = useCollectionOnce(
-    firebase.firestore().collection('Entries').where('user', '==', uid),
+    firebase
+      .firestore()
+      .collection('Entries')
+      .where('user', '==', getUser().uid),
     {
       snapshotListenOptions: { includeMetadataChanges: true },
     }
   );
 
+  useEffect(() => {
+    if (!value) return;
+    setSortedData(
+      value.docs.sort((a, b) =>
+        a.data().startTime > b.data().startTime ? -1 : 1
+      )
+    );
+  }, [value]);
+
   return (
     <Card>
       {error && <strong>Error: {JSON.stringify(error)}</strong>}
       {loading && <span>Collection: Loading...</span>}
-      {value && (
+      {sortedData && (
         <>
           <h4>Your Entries: </h4>
           <ListGroup variant='flush'>
-            {value.docs.map((doc) => (
+            {sortedData.map((doc) => (
               <EntryItem key={doc.id} data={doc.data()} />
             ))}
           </ListGroup>
--- src/components/Dashboard/Entries/EntryItem.js
+++ src/components/Dashboard/Entries/EntryItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import moment from 'moment';
 
 const EntryItem = ({ data }) => {
-  return <ListGroup>{data.project}</ListGroup>;
+  return (
+    <ListGroup.Item className='entry-row'>
+      <div className='entry-row-item entry-row-item-time'>
+        <div className='time-div time-div-date'>
+          {moment(data.startTime).format('MM/D/YY')}
+        </div>
+        <div className='time-div time-div-time'>
+          {moment(data.startTime).format('h:mma')}
+        </div>
+      </div>
+      <div className='entry-row-item'>{data.project || 'No Project'}</div>
+      <div className='entry-row-item'>{data.status}</div>
+    </ListGroup.Item>
+  );
 };
 
 export default EntryItem;
